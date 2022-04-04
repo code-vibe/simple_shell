8ff504ddb5a6b555c809a5f988bff6c833717590
Luis Manrique <luismanrique158158@gmail.com>
2022-04-04 00:33:32 +0000

    Adding main.h

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/wait.h>
+
+
+
+
+
+
+
+#endif /*MAIN_H*/
