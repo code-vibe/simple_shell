1d4248e364941c22d18eea54405181074f543ed6
Luis Manrique <luismanrique158158@gmail.com>
2022-04-11 12:29:14 +0000

    Adding EOF validation in function main

--- main.c
+++ main.c
@@ -28,22 +28,22 @@ int main(int argc __attribute__((unused)), char **argv __attribute__((unused)))
 
 		if (status_return)
 			write(STDOUT_FILENO, "#Shell_CL$ ", 11);
-
-
 		n = getline(&input_stdin, &size, stdin);
-
 		if (n == -1)
 		{
 			free(input_stdin);
 			break;
 		}
-
+		if (n == EOF)
+		{
+			free(input_stdin);
+			break;
+		}
 		if (validate_spaces(input_stdin))
 		{
 			free(input_stdin);
 			continue;
 		}
-
 		arguments = hsh_tokenizer(input_stdin);
 		if (*arguments[0] == '\0')
 			continue;
