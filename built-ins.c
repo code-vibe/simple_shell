5cfb565b96078ef324eac028fc1e3367ae3e940e
ChristianVaras <christianvaras.2020@gmail.com>
2022-04-10 22:15:12 -0500

    Adding function exit built in
    Adding function that print enviroment

--- built-ins.c
+++ built-ins.c
@@ -15,3 +15,27 @@ int hsh_exit(char **args __attribute__((unused)),
 	free(input_stdin);
 	exit(EXIT_SUCCESS);
 }
+/**
+ * hsh_env - Function that print enviromental
+ *
+ * @args: arguments
+ * @input_stdin: input of stdin
+ *
+ * Return: Always 1 (success)
+ */
+
+int hsh_env(char **args __attribute__((unused)),
+		char *input_stdin __attribute__((unused)))
+{
+	int i = 0;
+
+	if (environ[i] == NULL)
+	{
+		printf("%s", "The built in env is empty");
+		return (1);
+	}
+	for (i = 0; environ[i]; i++)
+		printf("%s\n", environ[i]);
+
+	return (1);
+}
