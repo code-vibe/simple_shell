7be104c83f617177ae71b1c9910589c2d0e09ca7
ChristianVaras <christianvaras.2020@gmail.com>
2022-04-11 18:34:04 -0500

    Adding setenv and unsetenv builtin's

--- built-ins.c
+++ built-ins.c
@@ -96,11 +96,65 @@ int hsh_cd(char **args, char *input_stdin, int *exit_status)
 	setenv("PWD", getcwd(s, sizeof(s)), 1);
 	return (1);
 }
+/**
+ * built_setenv - Command to set or overwrite a env variable.
+ * @args: argument array
+ * @input_stdin: input of stdin
+ * @exit_status: exit status
+ * Return: Return 1 if use a function, 0 otherwise.
+ */
 
+int hsh_setenv(char **args,  char *input_stdin, int *exit_status)
+{
+	int n_tokens = 0;
+
+	(void)input_stdin;
+	(void)exit_status;
+
+	while (args[n_tokens] != NULL)
+	{
+		n_tokens++;
+	}
+
+	if (n_tokens == 3)
+		setenv(args[1], args[2], 1);
+	else if (n_tokens < 3)
+		fprintf(stderr, "Try use \"setenv [KEY] [VALUE]\"\n");
+	else
+		fprintf(stderr, "Try use \"setenv [KEY] [VALUE]\"\n");
+
+	return (1);
+}
+/**
+ * built_unsetenv - Command of the simple shell eliminate a env var.
+ * @args: argument array
+ * @input_stdin: input of stdin
+ * @exit_status: exit status
+ * Return: Return 1 if use a function, 0 otherwise.
+ */
+
+int hsh_unsetenv(char **args,  char *input_stdin, int *exit_status)
+{
+	int n_tokens = 0;
+
+	(void)input_stdin;
+	(void)exit_status;
+
+	while (args[n_tokens] != NULL)
+        {
+                n_tokens++;
+        }
+	
+	if (n_tokens == 2)
+		unsetenv(args[1]);
+	else
+		fprintf(stderr, "Try use \"unsetenv [KEY]\"\n");
+
+	return (1);
+}
 /**
  * hsh_execute_builtins - Function that execute a
  * correct builtin command
- *
  * @args: arguments
  * @input_stdin: input of stdin
  * @argv: argv
@@ -119,6 +173,8 @@ int hsh_execute_builtins(char **args, char *input_stdin, char **argv,
 		{"exit", hsh_exit},
 		{"env", hsh_env},
 		{"cd", hsh_cd},
+		{"setenv", hsh_setenv},
+		{"unsetenv", hsh_unsetenv},
 		{NULL, NULL}
 	};
 
