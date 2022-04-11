d61118ea59abf814f063b26457e589477259c2bc
Luis Manrique <luismanrique158158@gmail.com>
2022-04-11 03:26:42 +0000

    Function that validate if path exists

--- main.c
+++ main.c
@@ -100,3 +100,58 @@ char **hsh_tokenizer(char *input)
 	tokens[position] = NULL;
 	return (tokens);
 }
+
+/**
+ * validate_input - Function that validate if path exists
+ *
+ * @arguments: arguments for command
+ * @argv: Array that contains the string values
+ * of things that were entered on the command line
+ * when I run this program
+ * Return: new arguments with complete path
+ */
+
+
+char *validate_input(char **arguments, char **argv)
+{
+	char *new_arguments, *first, *slash_argument = "/";
+	char **tokens_path, holder_env[1024];
+	int i = 0;
+
+	if (arguments[0][0] == '/')
+	{
+		new_arguments = arguments[0];
+		if ((access(new_arguments, F_OK) == -1))
+		{
+			printf("%s: 1: %s:  not found\n", argv[0], arguments[0]);
+			return ("Fail access");
+		}
+	}
+	else
+	{
+		strcpy(holder_env, getenv("PATH"));
+		tokens_path = tokenizer_path(holder_env);
+		while (tokens_path[i])
+		{
+			first = str_concat(slash_argument, arguments[0]);
+			new_arguments = str_concat(tokens_path[i], first);
+
+			if ((access(new_arguments, F_OK) == -1))
+			{
+				free(new_arguments);
+				free(first);
+			}
+			else
+			{
+				free(tokens_path);
+				free(first);
+				return (new_arguments);
+			}
+			i++;
+		}
+		free(tokens_path);
+		printf("%s: 1: %s:  not found\n", argv[0], arguments[0]);
+		return ("Fail access");
+	}
+	return (new_arguments);
+}
