033632d7d0861e8ea2d7ed63a26b9066a6e8de71
Luis Manrique <luismanrique158158@gmail.com>
2022-04-11 12:19:13 +0000

    Return last version

--- main.c
+++ main.c
@@ -52,10 +52,7 @@ int main(int argc __attribute__((unused)), char **argv __attribute__((unused)))
 		free(input_stdin);
 		free(arguments);
 	}
-	if (isatty(STDIN_FILENO))
-		printf("\n");
-	exit (98);
-	/*return (0);*/
+	return (0);
 }
 
 /**
