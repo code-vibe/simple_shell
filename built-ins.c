c8922fb97562ffc18b226c301ab3a83ea2a6309f
Luis Manrique <luismanrique158158@gmail.com>
2022-04-12 03:29:48 +0000

    Fix betty style in builtins commands

--- built-ins.c
+++ built-ins.c
@@ -96,62 +96,7 @@ int hsh_cd(char **args, char *input_stdin, int *exit_status)
 	setenv("PWD", getcwd(s, sizeof(s)), 1);
 	return (1);
 }
-/**
- * built_setenv - Command to set or overwrite a env variable.
- * @args: argument array
- * @input_stdin: input of stdin
- * @exit_status: exit status
- * Return: Return 1 if use a function, 0 otherwise.
- */
 
-int hsh_setenv(char **args,  char *input_stdin, int *exit_status)
-{
-	int n_tokens = 0;
-
-	(void)input_stdin;
-	(void)exit_status;
-
-	while (args[n_tokens] != NULL)
-	{
-		n_tokens++;
-	}
-
-	if (n_tokens == 3)
-		setenv(args[1], args[2], 1);
-	else if (n_tokens < 3)
-		fprintf(stderr, "Try use \"setenv [KEY] [VALUE]\"\n");
-	else
-		fprintf(stderr, "Try use \"setenv [KEY] [VALUE]\"\n");
-
-	return (1);
-}
-/**
- * built_unsetenv - Command of the simple shell eliminate a env var.
- * @args: argument array
- * @input_stdin: input of stdin
- * @exit_status: exit status
- * Return: Return 1 if use a function, 0 otherwise.
- */
-
-int hsh_unsetenv(char **args,  char *input_stdin, int *exit_status)
-{
-	int n_tokens = 0;
-
-	(void)input_stdin;
-	(void)exit_status;
-
-	while (args[n_tokens] != NULL)
-        {
-                n_tokens++;
-        }
-	
-	if (n_tokens == 2)
-		unsetenv(args[1]);
-	else
-		fprintf(stderr, "Try use \"unsetenv [KEY]\"\n");
-
-	return (1);
-}
 /**
  * hsh_execute_builtins - Function that execute a
  * correct builtin command
